f2921facbefb4cffc711d08fdf51d65a77ea75e6
AYU <star82620@gmail.com>
2023-04-18 15:59:40 +0800

    update/ set layout css

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,24 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
-      color: {
-        "primary-dark-rgb": "rgb(0, 20, 31)",
-        "primary-color-rgb": "rgb(0, 170, 255)",
-        "primary-light-rgb": "rgb(209, 240, 255)",
+      colors: {
+        "primary-dark": "rgb(0, 20, 31)",
+        "primary-color": "rgb(0, 170, 255)",
+        "primary-light": "rgb(209, 240, 255)",
         "success": "rgb(0, 255, 0)",
         "error": "rgb(255, 0, 0)",
         
       },
+      backgroundImage: {
+        "full-page-image": 'url("../img/BG2.JPG")',
+      },
+      boxShadow: {
+        "line": "0 0 0 1px",
 
-
+      },
+      spacing: {
+        "640px": "40rem",
+      },
     },
   },
   plugins: [],
